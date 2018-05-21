e5745f45899b0d457eb2fe5ef6510ace725f59ee
Hash&Salt <hello@hashandsalt.com>
2018-05-21 20:11:49 +0100

    Package up for release

---
+++ chopper.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ *
+ * Chopper Plugin for Kirby 2
+ *
+ * @version   0.0.1
+ * @author    James Steel <https://hashandsalt.com>
+ * @copyright James Steel <https://hashandsalt.com>
+ * @link      https://github.com/HashandSalt/chopper
+ * @license   MIT <http://opensource.org/licenses/MIT>
+ */
+
+require('lib/DOMLettersIterator.php');
+require('lib/DOMWordsIterator.php');
+require('lib/TruncateHTML.php');
+
+field::$methods['chopper'] = function($field, $length = 250, $type = 'words', $elipses = '…') {
+
+	$options = array(
+		'html' => $field,
+		'length' => $length,
+		'type' => $type, // also 'words'
+		'elipses' => $elipses
+	);
+
+	$result = '';
+
+	switch ($options['type']) {
+		case 'chars':
+			$result = TruncateHTML::truncateChars($options['html']->kirbytext(), $options['length'], $options['elipses']);
+			break;
+		default:
+			$result = TruncateHTML::truncateWords($options['html']->kirbytext(), $options['length'], $options['elipses']);
+			break;
+	}
+
+	$keep = c::get('chopper.keep', '<p><a><strong><em><sub><sup><blockquote><figure><img>');
+
+	$result = strip_tags($result, $keep);
+
+	return $result;
+};
