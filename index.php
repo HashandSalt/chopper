b49207827d35e39b9ab92dba2b661445815a11cc
HashandSalt <hello@hashandsalt.com>
2022-04-16 11:15:06 +0100

    Refactor for modern PHP

--- index.php
+++ index.php
@@ -1,62 +1,36 @@
 <?php
-/**
- *
- * Chopper Plugin for Kirby 3
- *
- * @version   2.0.3
- * @author    James Steel <https://hashandsalt.com>
- * @copyright James Steel <https://hashandsalt.com>
- * @link      https://github.com/HashandSalt/chopper
- * @license   MIT <http://opensource.org/licenses/MIT>
- */
 
-@include_once __DIR__ . '/vendor/autoload.php';
+load([
+    'hashandsalt\\chopper\\chopper' => 'src/Chopper.php'
+], __DIR__);
 
-require('lib/DOMLettersIterator.php');
-require('lib/DOMWordsIterator.php');
-require('lib/TruncateHTML.php');
-
+use HashAndSalt\Chopper\Chopper;
 
 Kirby::plugin('hashandsalt/chopper', [
-	// Options
-	'options' => [
-		'keep' => '<p><a><strong><em><sub><sup><blockquote><figure><img><h1><h2><h3><h4><h5><h6>',
-		'suffix' => '…'
-  ],
+   
 
-	'fieldMethods' => [
-		'chopper' => function ($field, $length = 250, $type = 'words', $suffix = '') {
+    'options' => [
+        'ellipsis' => '…',
+        'exact' => true,
+        'html' => true,
+        'trimWidth' => false,
+        'keep' => '<p><a><strong><em><sub><sup><blockquote><figure><img><h1><h2><h3><h4><h5><h6>',
+    ],
 
+    'fieldMethods' => [
+		'chopper' => function ($field, $length = 250, $options = []) {
+            
+            $options += [
+                'ellipsis' => option('hashandsalt.chopper.ellipsis'), 'exact' => option('hashandsalt.chopper.exact'), 'html' => option('hashandsalt.chopper.html'), 'trimWidth' => option('hashandsalt.chopper.trimWidth'),
+            ];
 
-		$chopper = $field->kt();
-		$suffix = !empty($suffix) ? $suffix : option('hashandsalt.chopper.suffix');
+            $chopper = $field->kt();
+            $field = Chopper::truncate($chopper, $length, $options);
 
-		if($type == 'words') {
-			$field = TruncateHTML::truncateWords($chopper, $length, $suffix);
-		} else {
-			$field = TruncateHTML::truncateChars($chopper, $length, $suffix);
-		}
+            $field = strip_tags($field, option('hashandsalt.chopper.keep'));
 
+            return $field;		
 	}
-
-
  ]
 
-
 ]);
-
-class Chopper {
-    public static function excerpt($text, $length = 250, $type = 'words', $elipses = '…') {
-		$chopped = strip_tags($text, option('hashandsalt.chopper.keep'));
-		
-		
-
-        if($type == 'words') {
-            $field = TruncateHTML::truncateWords($chopped, $length, $elipses);
-        } else {
-            $field = TruncateHTML::truncateChars($chopped, $length, $elipses);
-        }
-
-        return $chopped;
-    }
-}
